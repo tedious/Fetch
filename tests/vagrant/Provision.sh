ca5aaa6a366531a2c3812ba4ff2bbc2365cf1d55
Robert Hafner <tedivm@tedivm.com>
2013-12-01 15:53:05 -0800

    Initial Vagrant setup

---
+++ tests/vagrant/Provision.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+
+# Install and Configure Dovecot
+
+if which dovecot > /dev/null; then
+    echo 'Dovecot is already installed'
+else
+    echo 'Installing Dovecot'
+    sudo apt-get -qq -y install dovecot-imapd dovecot-pop3d
+    sudo touch /etc/dovecot/local.conf
+    sudo echo 'mail_location = maildir:/home/%u/Maildir' >> /etc/dovecot/local.conf
+    sudo echo 'disable_plaintext_auth = no' >> /etc/dovecot/local.conf
+    sudo restart dovecot
+fi
+
+
+# Create "testuser"
+
+if getent passwd testuser > /dev/null; then
+    echo 'testuser already exists'
+else
+    echo 'Creating testuser'
+    sudo useradd testuser -m -s /bin/bash
+    echo "testuser:applesauce"|sudo chpasswd
+fi
