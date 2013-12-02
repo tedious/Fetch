13514054d9f555ba852dcc4ea85a67ed177bf316
Robert Hafner <tedivm@tedivm.com>
2013-12-01 23:05:55 -0800

    Updated system hostname to tedivm.com

--- tests/vagrant/Postfix.sh
+++ tests/vagrant/Postfix.sh
@@ -4,7 +4,7 @@ if which postfix > /dev/null; then
     echo 'Postfix is already installed'
 else
     echo 'Installing Postfix'
-    sudo debconf-set-selections <<< "postfix postfix/mailname string precise64"
+    sudo debconf-set-selections <<< "postfix postfix/mailname string tedivm.com"
     sudo debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Local only'"
     sudo apt-get -qq update
     sudo apt-get -qq -y install postfix mailutils
