9d26773f7fbf863e5386ecb1dae7c7b905fe05c7
Robert Hafner <tedivm@tedivm.com>
2014-04-19 23:42:19 -0700

    use bin/php-cs-fixer instead of bundle version

--- tests/runTests.sh
+++ tests/runTests.sh
@@ -14,4 +14,4 @@ echo ''
 echo ''
 echo 'Testing for Coding Styling Compliance.'
 echo 'All code should follow PSR standards.'
-./vendor/fabpot/php-cs-fixer/php-cs-fixer fix ./ --level="all" -vv --dry-run
+./vendor/bin/php-cs-fixer fix ./ --level="all" -vv --dry-run
