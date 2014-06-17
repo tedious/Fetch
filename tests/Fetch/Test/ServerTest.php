7926e5c9c636ffaefaa48455677299778eecdcc1
Alex Kavon <alexkavon@users.noreply.github.com>
2014-06-17 14:19:49 -0600

    Fixed spacing/tab issue...

--- tests/Fetch/Test/ServerTest.php
+++ tests/Fetch/Test/ServerTest.php
@@ -139,12 +139,12 @@ class ServerTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('Sent', $server->getMailBox());
     }
 
-	public function testGetMailboxes()
-	{
-		$box = ["INBOX","Sent","Flagged Email"];
-		$server = Static::getServer();
-		$this->assertEquals($box, $server->getMailBoxes());
-	}
+    public function testGetMailboxes()
+    {
+        $box = ["INBOX","Sent","Flagged Email"];
+        $server = Static::getServer();
+        $this->assertEquals($box, $server->getMailBoxes());
+    }
 
     public function testSetMailBox()
     {
