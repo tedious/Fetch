e4cc900f3606c37d3c6db9cbe92cc8cf97ebfd9c
Alex Kavon <alexkavon@users.noreply.github.com>
2014-06-18 16:50:52 -0600

    Added deleteMailBox() function

--- src/Fetch/Server.php
+++ src/Fetch/Server.php
@@ -462,4 +462,16 @@ class Server
     {
         return imap_createmailbox($this->getImapStream(), $this->getServerSpecification() . $mailbox);
     }
+    
+    /**
+     * Deletes the given mailbox.
+     * 
+     * @param $mailbox
+     * 
+     * @return bool
+     */
+     public function deleteMailBox($mailbox)
+     {
+         return imap_deletemailbox($this->getImapStream(), $this->getServerSpecification() . $mailbox);
+     }
 }
