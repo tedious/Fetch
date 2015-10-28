0fb94d262d57fa6a301bb55991f23a9e8e9ea0d9
Jascha Luelsdorf <jascha@jluelsdorf.de>
2015-10-28 12:51:07 +0100

    Added switch to avoid 'incomplete multibyte character' when converting utf-8 to utf-8

--- src/Fetch/MIME.php
+++ src/Fetch/MIME.php
@@ -37,7 +37,9 @@ final class MIME
         foreach (imap_mime_header_decode($text) as $word) {
             $ch = 'default' === $word->charset ? 'ascii' : $word->charset;
 
-            $result .= iconv($ch, $targetCharset, $word->text);
+            if (strtoupper($ch) != strtoupper($targetCharset)) {
+                $result .= iconv($ch, $targetCharset, $word->text);
+            }
         }
 
         return $result;
