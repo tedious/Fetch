d18add18ea8031825780d99979dcf12cee859de6
deemyboy <deemyboy@users.noreply.github.com>
2016-11-15 09:39:45 +0000

    update message to include changes

--- src/Fetch/Message.php
+++ src/Fetch/Message.php
@@ -229,50 +229,55 @@ class Message
 
         /* First load the message overview information */
 
-        if(!is_object($messageOverview = $this->getOverview()))
+        if (!is_object($messageOverview = $this->getOverview()))
 
             return false;
-
-        $this->subject = MIME::decode($messageOverview->subject, self::$charset);
-        $this->date    = strtotime($messageOverview->date);
-        $this->size    = $messageOverview->size;
-
-        foreach (self::$flagTypes as $flag)
-            $this->status[$flag] = ($messageOverview->$flag == 1);
-
-        /* Next load in all of the header information */
-
-        $headers = $this->getHeaders();
-
-        if (isset($headers->to))
-            $this->to = $this->processAddressObject($headers->to);
-
-        if (isset($headers->cc))
-            $this->cc = $this->processAddressObject($headers->cc);
-
-        if (isset($headers->bcc))
-            $this->bcc = $this->processAddressObject($headers->bcc);
-
-        if (isset($headers->sender))
-            $this->sender = $this->processAddressObject($headers->sender);
-
-        $this->from    = isset($headers->from) ? $this->processAddressObject($headers->from) : array('');
-        $this->replyTo = isset($headers->reply_to) ? $this->processAddressObject($headers->reply_to) : $this->from;
-
-        /* Finally load the structure itself */
-
-        $structure = $this->getStructure();
-
-        if (!isset($structure->parts)) {
-            // not multipart
-            $this->processStructure($structure);
+//        echo 'the function that checks the subject >'.$this->subject;
+//        print_r($messageOverview);
+        if (!($this->hasSubject($messageOverview))) { //nadeem
+            $this->requestNewEmailWithSubject($messageOverview->from); //nadeem
         } else {
-            // multipart
-            foreach ($structure->parts as $id => $part)
-                $this->processStructure($part, $id + 1);
-        }
+            $this->subject = MIME::decode($messageOverview->subject, self::$charset);
+            $this->date = strtotime($messageOverview->date);
+            $this->size = $messageOverview->size;
 
-        return true;
+            foreach (self::$flagTypes as $flag)
+                $this->status[$flag] = ($messageOverview->$flag == 1);
+
+            /* Next load in all of the header information */
+
+            $headers = $this->getHeaders();
+
+            if (isset($headers->to))
+                $this->to = $this->processAddressObject($headers->to);
+
+            if (isset($headers->cc))
+                $this->cc = $this->processAddressObject($headers->cc);
+
+            if (isset($headers->bcc))
+                $this->bcc = $this->processAddressObject($headers->bcc);
+
+            if (isset($headers->sender))
+                $this->sender = $this->processAddressObject($headers->sender);
+
+            $this->from = isset($headers->from) ? $this->processAddressObject($headers->from) : array('');
+            $this->replyTo = isset($headers->reply_to) ? $this->processAddressObject($headers->reply_to) : $this->from;
+
+            /* Finally load the structure itself */
+
+            $structure = $this->getStructure();
+
+            if (!isset($structure->parts)) {
+                // not multipart
+                $this->processStructure($structure);
+            } else {
+                // multipart
+                foreach ($structure->parts as $id => $part)
+                    $this->processStructure($part, $id + 1);
+            }
+
+            return true;
+        }
     }
 
     /**
@@ -794,4 +799,51 @@ class Message
 
         return $returnValue;
     }
+
+    /*  nadeem additions to fix empty subject breaking seraph 1.11.16 */
+
+
+    /**
+     * Checks to see if email subject is not blank
+     * @author nadeem
+     * @date 10.11.16
+     *
+     * @params $message - the email message to be tested
+     * @return bool§
+     */
+    public function hasSubject($message)
+    {
+        if(property_exists($message,'subject'))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    /**
+     * sends email to sender to tell them that their email
+     * didn't have a subject and has not been booked into
+     * seraph
+     *
+     * @author nadeem
+     * @date 10.11.16
+     *
+     * @params $from -> email address of sender
+     */
+    protected function requestNewEmailWithSubject($from)
+    {
+        echo 'subject missing - sender: ' . $from;
+        $admin_email = "nadeem@computerassistance.co.uk" . ',' . $from;
+//        $email = $_REQUEST['email'];
+        $subject = 'email without a subject to helpdesk';
+//        $comment = $_REQUEST['comment'];
+        $body = "Your request to book a ticket has been declined \nreason: no subject. \nplease add a subject to your email and re-send it, thanks. \nHelpdesk @ CA";
+        //send email
+        mail($admin_email, $subject, $body);
+    }
+
+
 }
