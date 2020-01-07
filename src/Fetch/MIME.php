8530a9133601d9da63516b6776e626216778222a
Marnu Lombard <marnulombard@gmail.com>
2020-01-07 08:10:40 +0200

    [bugfix] Prevent the attempt to use encodings that `mb_convert_encoding()` can't use

--- src/Fetch/MIME.php
+++ src/Fetch/MIME.php
@@ -37,7 +37,8 @@ final class MIME
         foreach (imap_mime_header_decode($text) as $word) {
             $ch = 'default' === $word->charset ? 'ascii' : $word->charset;
             $text = $word->text;
-            if (function_exists('mb_convert_encoding')) {
+            // Use the possible false return of `mb_encoding_aliases()` to detect whether we can process the encoding
+            if (function_exists('mb_convert_encoding') && @mb_encoding_aliases($ch)) {
                 // This will strip any unrecognised characters and ensure we avoid
                 // "Detected an incomplete multibyte character in input string" errors
                 $text = mb_convert_encoding($text, $ch, $ch);
